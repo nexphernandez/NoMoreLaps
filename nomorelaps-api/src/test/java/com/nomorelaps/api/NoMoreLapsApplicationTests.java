94b7f7ea0b134421a604585099ea35a5120e06bd
nexphernandez <nicolaseh3b1112@gmail.com>
2026-02-07 18:03:57 +0000

    Se crea la estructura de la api back

---
+++ nomorelaps-api/src/test/java/com/nomorelaps/api/NoMoreLapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomorelaps.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoMoreLapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
