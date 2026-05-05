d32f982680f65dd8b572a0cafce4c3879253ecbe
nexphernandez <nicolaseh3b1112@gmail.com>
2026-05-05 14:04:28 +0100

    Se implementa la clase ApiKeyFilter para la integracion con las empresas #39

--- api-back/src/main/java/com/nomorelaps/adapters/in/api/ParkingRequest.java
+++ api-back/src/main/java/com/nomorelaps/adapters/in/api/ParkingRequest.java
@@ -26,6 +26,9 @@ public class ParkingRequest {
     private LocalDateTime openingTime;
     private LocalDateTime closingTime;
     private Double pricePerHour;
+    private Long companyId;
+    private Double sanctionAmount;
+    private Integer sanctionIntervalInMinutes;
 
     /**
      * Empty constructor
@@ -41,9 +44,14 @@ public class ParkingRequest {
      * @param longitude of the parking
      * @param openingTime of the parking that day
      * @param closingTime of the parking that day
+     * @param pricePerHour price per hour
+     * @param companyId company owner id
+     * @param sanctionAmount sanction amount
+     * @param sanctionInterval sanction interval
      */
     public ParkingRequest(String address, String name, Double latitude, Double longitude, 
-                          LocalDateTime openingTime, LocalDateTime closingTime, Double pricePerHour) {
+                          LocalDateTime openingTime, LocalDateTime closingTime, Double pricePerHour,
+                          Long companyId, Double sanctionAmount, Integer sanctionInterval) {
         this.address = address;
         this.name = name;
         this.latitude = latitude;
@@ -51,6 +59,9 @@ public class ParkingRequest {
         this.openingTime = openingTime;
         this.closingTime = closingTime;
         this.pricePerHour = pricePerHour;
+        this.companyId = companyId;
+        this.sanctionAmount = sanctionAmount;
+        this.sanctionIntervalInMinutes = sanctionInterval;
     }
 
     public String getAddress() {
@@ -107,6 +118,30 @@ public class ParkingRequest {
 
     public void setPricePerHour(Double pricePerHour) {
         this.pricePerHour = pricePerHour;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public Double getSanctionAmount() {
+        return sanctionAmount;
+    }
+
+    public void setSanctionAmount(Double sanctionAmount) {
+        this.sanctionAmount = sanctionAmount;
+    }
+
+    public Integer getSanctionIntervalInMinutes() {
+        return sanctionIntervalInMinutes;
+    }
+
+    public void setSanctionIntervalInMinutes(Integer sanctionIntervalInMinutes) {
+        this.sanctionIntervalInMinutes = sanctionIntervalInMinutes;
     }
 
 }
